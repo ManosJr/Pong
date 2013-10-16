568f97e5490092fdb9e825636b9900d9735ef928
jamesmoore <jamesmoore@10.0.1.90>
2013-10-16 10:11:54 -0700

    Asteroids copy 1.2

---
+++ Asteroids/src/asteroids/Main.java
@@ -0,0 +1,13 @@
+package asteroids;
+
+/**
+ * copyright 2013 james moore 
+ * version 131016A
+ */
+public class Main
+{
+     public static void main(String[] args)
+    {
+        new Controller().getGoing();
+    }
+}
